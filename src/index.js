42527c4cfee8e39372373ef2228fb91e89a9daf7
shazaq03 <shazaq03@gmail.com>
2022-12-20 13:23:41 +0530

    fixed bug

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/styles.css';
 import App from './App';
-import { hashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Store } from './StoreandSlices/Store';
 import reportWebVitals from './reportWebVitals';
