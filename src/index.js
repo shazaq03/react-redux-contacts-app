4fc60c31c13259171d450a8a87c75040706e1d81
shazaq03 <shazaq03@gmail.com>
2022-12-20 13:17:31 +0530

    gh-pages setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/styles.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { hashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Store } from './StoreandSlices/Store';
 import reportWebVitals from './reportWebVitals';
